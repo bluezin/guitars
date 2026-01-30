6a0b8985bbdb489f785b0512ccf4522852089f90
yadira <yadiraco304@gmail.com>
2026-01-30 12:33:26 -0500

    fix: remove config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "46.225.3.58"
+  # config.hosts << "46.225.3.58"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
